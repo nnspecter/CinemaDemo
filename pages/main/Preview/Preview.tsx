86c3e9dff0290c209c5c3ec344ceeb218e6db4c1
nnspecter <nevozmozhno0000@email.com>
2025-08-25 19:51:13 +0300

    закончил делать свайпер

--- pages/main/Preview/Preview.tsx
+++ pages/main/Preview/Preview.tsx
@@ -1,33 +1,54 @@
+"use client";
 import React from 'react'
 import styles from "./Preview.module.scss"
-import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
+// Импорты CSS
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import PreviewSlide from './PreviewSlide';
 
 const Preview = () => {
   return (
     <div className={styles.preview}>
-    
-        <div className={styles.prewiewImage}>
-            <Image
-            src="/images/preview/previewGradient.png"
-            alt='Back'
-            fill
-            className={styles.img}
-            />
-        </div>
-        <div className={styles.onImageBlock}>
-            <div className={styles.text}>
-                <p>2019</p>
-                <h1>Интерстеллар возрождение</h1>
-                <p>
-                    Сюжет рассказывает о бывшем пилоте НАСА Купере,
-                    который вместе с группой исследователей отправляется
-                    через червоточину в космос, чтобы найти новый дом
-                    для человечества, стоящего на грани вымирания
-                    из-за экологической катастрофы на Земле.
-                </p>
-            </div>
-            <img src="images/preview/interstellarLogo.jpg" alt="KAPTINKA"></img>
-        </div>
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        modules={[Autoplay]} 
+        autoplay={{
+          delay: 6000, // каждые 3 секунды
+          disableOnInteraction: false, // не останавливать при ручном свайпе
+        }}
+        loop={true}
+        
+      >
+        <SwiperSlide>
+          <PreviewSlide 
+          imgBackground="/images/preview/previewGradient.png"
+          year="2023"
+          name="Интерстеллар возрождение"
+          desc="Сюжет рассказывает о бывшем пилоте НАСА Купере, который вместе с группой исследователей отправляется через червоточину в космос, чтобы найти новый дом для человечества, стоящего на грани вымирания из-за экологической катастрофы на Земле."
+          poster="/images/preview/InterstellarLogo.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <PreviewSlide 
+          imgBackground="/images/preview/AlienGradient.png"
+          year="2024"
+          name="Чужой Ромул"
+          desc="Экипаж космического буксира «Ностромо» получает сигнал бедствия с неизвестной планеты. Прибыв на место, они находят гигантский инопланетный корабль и таинственные коконы. Один из членов команды подвергается нападению иноплантической формы жизни, которая позже вырывается наружу и начинает охоту на людей."
+          poster="/images/preview/AlienLogo.jpg"/>
+        </SwiperSlide>
+        <SwiperSlide>
+          <PreviewSlide 
+          imgBackground="/images/preview/DeadpoolGradient.png"
+          year="2024"
+          name="Дедпул"
+          desc="Бывший спецназовец Уэйд Уилсон узнаёт о смертельной болезни и соглашается на эксперимент, обещающий исцеление. Но вместо обычного излечения он получает невероятную способность к регенерации, изуродованную внешность и безумное чувство юмора."
+          poster="/images/preview/DeadPoolLogo.jpg"/>
+        </SwiperSlide>
+      </Swiper>
     </div>
   )
 }
---
+++ pages/main/Preview/PreviewSlide.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from "./PreviewSlide.module.scss"
+
+interface PreviewSlideProps {
+  imgBackground: string; // путь к фону
+  year: number | string; // год
+  name: string;          // название
+  desc: string;          // описание
+  poster: string;        // постер / логотип
+}
+
+
+const PreviewSlide: React.FC<PreviewSlideProps> = ({imgBackground, year, name, desc, poster}) => {
+  return (
+    <div className={styles.previewSlide}>
+    
+        <div className={styles.prewiewImage}>
+            <Image
+            src={imgBackground}
+            alt='Back'
+            fill
+            priority
+            className={styles.img}
+            />
+        </div>
+        <div className={styles.onImageBlock}>
+            <div className={styles.text}>
+                <p>{year}</p>
+                <h1>{name}</h1>
+                <p>{desc}</p>
+            </div>
+            <img src={poster} alt="KAPTINKA"></img>
+        </div>
+    </div>
+  )
+}
+
+export default PreviewSlide
