5a7f8ce51d244f89db6eb2edb0f270d737ccbd3f
Neo <nnspecter@gmail.com>
2025-09-03 12:25:18 +0300

    пробуем исправить ошибку

--- app/api/cinema/movies/route.ts
+++ app/api/cinema/movies/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-
+export const dynamic = "force-dynamic";
 export async function GET() {
   try {
     const response = await fetch("https://bush-cinema.onrender.com/api/cinema/movies");
