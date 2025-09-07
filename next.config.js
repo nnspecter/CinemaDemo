4fe10b4ca7f01ae13ad236a5401c4f990d2c66d7
Neo <nnspecter@gmail.com>
2025-09-07 10:31:31 +0300

    тест ссылок

--- next.config.js
+++ next.config.js
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.ibb.co.com", // ✅ добавляем домен
+        hostname: "i.ibb.com",
       },
     ],
-  }
+    unoptimized: true, // для GitHub Pages
+  },
+  output: 'export', // экспорт статического билда
+  basePath: isProd ? '/CinemaDemo' : '', // ⚡ замени repo-name на имя репозитория
+  assetPrefix: isProd ? '/CinemaDemo/' : '', // чтобы пути к ассетам были правильные
 }
 
 module.exports = nextConfig
